944246227c75ba95194db9ff3270d23c7b182e46
mahdi-esmaeelnezhad <mahdiesmaeelnezhaddeveloper@gmail.com>
2022-10-25 23:55:39 +0330

    Upload simple PWA project on Github

---
+++ spa/asset/js/about.js
@@ -0,0 +1,31 @@
+const text2 = () => {
+    return `
+    <div class="org-info">
+    <div class="title-about">
+        <h1>Buy Web Hosting That Makes Building Any Website Easy</h1>
+        <p>With a 99.9% uptime guarantee, industry-leading technology, and responsive customer support, Hostinger can offer a reliable and high-performance shared hosting service for everyone. No matter where you are on your site-building journey.</p>
+    </div>
+    <div class="website-builder">
+        <div class="website-builder-img">
+            <img src="./asset/img/website-builder@2x-6c6e01bce0 (1).png" class="img-res" alt="website builder">
+        </div>
+        <div class="website-builder-text">
+            <h2>Website Builder</h2>
+            <p>Build a website with no coding skills with our drag-and-drop builder. Simply choose a template, insert your desired elements, and enjoy your new website. It doesn't get any easier than that.</p>
+        </div>
+    </div>
+    <div class="website-builder">
+        <div class="website-builder-text">
+            <h2>High-Quality Web Hosting for Everyone</h2>
+            <p>
+                With a 99.9% uptime guarantee, industry-leading technology, and responsive customer support, Hostinger can offer a reliable and high-performance shared hosting service for everyone. No matter where you are on your site-building journey.</p>
+        </div>
+        <div class="website-builder-img">
+            <img src="./asset/img/re.png" class="img-res" alt="website builder">
+        </div>  
+    </div>
+</div>
+    `
+}
+
+export default text2
---
+++ spa/asset/js/call.js
@@ -0,0 +1,29 @@
+const text3 = () => {
+    return `
+    <div class="org-call">
+    <div class="call">
+        <span>Call : </span>
+        <div>
+            <i class="fa-solid fa-phone"></i>
+            <a href="tell">+1-55346899</a>
+        </div>
+    </div>
+    <div class="email">
+        <span>Email : </span>
+        <div>
+            <i class="fa-solid fa-envelope"></i>
+            <span>BestHost@email.com</span>
+        </div>
+    </div>
+    <div class="address">
+        <span>Address :</span>
+        <div>
+            <i class="fa-solid fa-location-dot"></i>
+            <span>fars-sreet P3</span>
+        </div>
+    </div>
+</div>
+    `
+}
+
+export default text3
